f62354dda86a5bbfbbf3a2b030d630fff2368c3c
zhangjihe <jihe971004@163.com>
2022-05-10 10:07:59 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("张霁赫要加油啊");
+        System.out.println("张霁赫要开心啊");
     }
 }
