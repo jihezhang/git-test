250e52ec7c88c88e8fd963ed2e1c970af52d48aa
zhangjihe <jihe971004@163.com>
2022-05-10 10:26:00 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("张霁赫要开心啊");
         System.out.println("张霁赫要牛逼啊");
         System.out.println("正常合并分支");
+        System.out.println("不正常合并分支 hot fix");
     }
 }
